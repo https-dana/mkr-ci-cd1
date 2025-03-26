790d8cac6c2fbae8a9cd09db66f1e5ba25fa4141
https-dana <bogdana.kolesnyk@gmail.com>
2025-03-26 14:15:27 +0200

    Додано функцію count_sentences для підрахунку речень

--- mkr1.py
+++ mkr1.py
@@ -1,4 +1,14 @@
+import re
+
+
+
 def read_file(file_path):
     """Зчитує текстовий файл і повертає його вміст."""
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
+    
+def count_sentences(text):
+    """Рахує кількість речень у тексті."""
+    sentence_endings = re.compile(r'[.!?]+')
+    sentences = sentence_endings.split(text)
+    return sum(1 for s in sentences if s.strip())
